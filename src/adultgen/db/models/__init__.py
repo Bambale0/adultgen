12e51dfa0fb4412e2f99f9c433abc8cb1742f5f6
Bambale0 <83171474+Bambale0@users.noreply.github.com>
2026-08-04 23:03:39 +0300

    Add ORM models package

---
+++ src/adultgen/db/models/__init__.py
@@ -0,0 +1,62 @@
+"""ORM models registry.
+
+Importing this package loads all model modules so Alembic can see metadata.
+"""
+
+from adultgen.db.models.audit import AdminAuditEvent
+from adultgen.db.models.broadcasts import Broadcast, BroadcastRecipient
+from adultgen.db.models.generations import GenerationTask, SceneTake
+from adultgen.db.models.media import MediaAsset
+from adultgen.db.models.moderation import ModerationCase
+from adultgen.db.models.payments import PaymentOrder, PaymentWebhookProcessing, PaymentWebhookRaw
+from adultgen.db.models.projects import AvatarProfile, AvatarReference, Project, Scene, SceneReference
+from adultgen.db.models.publications import (
+    FeedEvent,
+    Publication,
+    PublicationLike,
+    RemixSource,
+    SavedPublication,
+    UserProfile,
+)
+from adultgen.db.models.referrals import (
+    PartnerCommission,
+    PartnerPayoutRequest,
+    PartnerWallet,
+    ReferralRelation,
+)
+from adultgen.db.models.users import AdultConsent, TelegramChannel, User, UserChannelActivity
+from adultgen.db.models.wallets import Wallet, WalletEntry
+
+__all__ = [
+    "AdminAuditEvent",
+    "AdultConsent",
+    "AvatarProfile",
+    "AvatarReference",
+    "Broadcast",
+    "BroadcastRecipient",
+    "FeedEvent",
+    "GenerationTask",
+    "MediaAsset",
+    "ModerationCase",
+    "PartnerCommission",
+    "PartnerPayoutRequest",
+    "PartnerWallet",
+    "PaymentOrder",
+    "PaymentWebhookProcessing",
+    "PaymentWebhookRaw",
+    "Project",
+    "Publication",
+    "PublicationLike",
+    "ReferralRelation",
+    "RemixSource",
+    "SavedPublication",
+    "Scene",
+    "SceneReference",
+    "SceneTake",
+    "TelegramChannel",
+    "User",
+    "UserChannelActivity",
+    "UserProfile",
+    "Wallet",
+    "WalletEntry",
+]
