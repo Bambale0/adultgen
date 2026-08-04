21258bf3bd534c31cb8691bea95d319c7e8fd86b
Bambale0 <83171474+Bambale0@users.noreply.github.com>
2026-08-04 23:12:28 +0300

    Sort Alembic imports

--- alembic/env.py
+++ alembic/env.py
@@ -5,15 +5,12 @@ from __future__ import annotations
 import asyncio
 from logging.config import fileConfig
 
+import adultgen.db.models  # noqa: F401
+from adultgen.config import get_settings
+from adultgen.db.base import Base
 from alembic import context
 from sqlalchemy import pool
 from sqlalchemy.ext.asyncio import async_engine_from_config
-
-from adultgen.config import get_settings
-from adultgen.db.base import Base
-
-# Import all models so metadata is populated.
-import adultgen.db.models  # noqa: F401
 
 config = context.config
 
