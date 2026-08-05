abe705ffc98d7eba1e1d2d0791625a51affe5294
Bambale0 <83171474+Bambale0@users.noreply.github.com>
2026-08-05 22:46:28 +0300

    Route admin path to standalone panel

--- apps/web_app/src/main.tsx
+++ apps/web_app/src/main.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+import { AdminPanel } from './AdminPanel';
 import { App } from './App';
 import './styles.css';
 
+const rootComponent = window.location.pathname === '/admin' ? <AdminPanel /> : <App />;
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    {rootComponent}
   </React.StrictMode>,
 );
