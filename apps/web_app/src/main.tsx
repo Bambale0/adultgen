acd152355d534410a00d92cfe4500fd0af0edd73
Bambale0 <83171474+Bambale0@users.noreply.github.com>
2026-08-06 12:32:11 +0300

    Load UX polish stylesheet

--- apps/web_app/src/main.tsx
+++ apps/web_app/src/main.tsx
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom/client';
 import { AdminPanel } from './AdminPanel';
 import { RoutedUserApp } from './RoutedUserApp';
 import './styles.css';
+import './ux-polish.css';
 import './admin.css';
 
 const isAdminRoute = window.location.pathname.startsWith('/admin');
