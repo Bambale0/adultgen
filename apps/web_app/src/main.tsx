a77e71d030db683607d85d3c10a540e4e3d81a63
Bambale0 <83171474+Bambale0@users.noreply.github.com>
2026-08-15 00:42:12 +0300

    fix: preserve frontend platform contracts

--- apps/web_app/src/main.tsx
+++ apps/web_app/src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { AdminPanel } from './AdminPanel';
 import { RoutedUserApp } from './RoutedUserApp';
+import './admin.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
